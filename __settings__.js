4f8ac3c8d2fd931f8c747321ad375109e9cb66bd
Alex <voronko20172017@gmail.com>
2025-06-03 13:26:31 +0300

    update

--- __settings__.js
+++ __settings__.js
@@ -8,7 +8,7 @@ window.CONTEXT_OPTIONS = {
     'deviceTypes': [`webgl2`, `webgl1`],
     'powerPreference': "high-performance"
 };
-window.SCRIPTS = [ 232627677 ];
+window.SCRIPTS = [  ];
 window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
